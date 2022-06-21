70d6b7878dc2039cfdc84944bb04edaa613e7d98
samson <sunnext2010@gmail.com>
2022-06-21 16:12:40 +0100

    add location

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../layout';
+import Location from '../userLocation';
 import UserAccount from './UserAccount';
 
 const Home = () => {
     return (
         <Layout>
+            <Location />
             <UserAccount />
         </Layout>
     );
